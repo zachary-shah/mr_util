65ed36e9138bda3cb12b45593d8ddac81bbab346
zachary-shah <zshah9@stanford.edu>
2026-02-10 22:59:35 -0800

    make grid directly on device

--- src/mr_util/utils.py
+++ src/mr_util/utils.py
@@ -264,7 +264,8 @@ def rotation_matrix(axis: torch.Tensor,
 
 
 def gen_grd(
-    im_size: tuple, fovs: Optional[tuple] = None, balanced: Optional[bool] = False
+    im_size: tuple, fovs: Optional[tuple] = None, balanced: Optional[bool] = False,
+    device: Optional[torch.device] = None
 ) -> torch.Tensor:
     """
     Generates a grid of points given image size and FOVs
@@ -281,17 +282,21 @@ def gen_grd(
     grd : torch.Tensor
         grid of points with shape (*im_size, len(im_size))
     """
+    kwargs = {}
+    if device is not None:
+        kwargs['device'] = device
+        
     if fovs is None:
         fovs = (1,) * len(im_size)
     if balanced:
         lins = [
-            fovs[i] * torch.linspace(-1 / 2, 1 / 2, im_size[i])
+            fovs[i] * torch.linspace(-1 / 2, 1 / 2, im_size[i], **kwargs)
             for i in range(len(im_size))
         ]
     else:
         lins = [
             fovs[i]
-            * torch.arange(-(im_size[i] // 2), im_size[i] // 2 + (im_size[i] % 2))
+            * torch.arange(-(im_size[i] // 2), im_size[i] // 2 + (im_size[i] % 2), **kwargs)
             / (im_size[i])
             for i in range(len(im_size))
         ]
@@ -394,7 +399,7 @@ def _spatial_resize(
         )
         method = "bilinear"
 
-    grd = 2 * gen_grd(im_size, balanced=True).to(x.device).flip(-1)
+    grd = 2 * gen_grd(im_size, balanced=True, device=x.device).flip(-1)
     grd = grd[None].repeat_interleave(x.shape[0], dim=0)
 
     def gs(x: torch.Tensor) -> torch.Tensor:
